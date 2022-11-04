30a08f7cbe5ca3880df49ce1cd086e27e50ce3cd
Tom Keene <tom@theanthillsocial.co.uk>
2022-11-04 11:34:13 +0000

    Quake update

--- week7-api/earthquake/sketch-timer.js
+++ week7-api/earthquake/sketch-timer.js
@@ -3,30 +3,34 @@ let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.cs
 
 function setup() {
   createCanvas(500, 400);
-  table = loadTable(url, 'csv', 'header');
+  //table = loadTable(url, 'csv', 'header');
 }
 
 function draw(){
     background(200);
 
-    // SETUP DEFAULT VARIABLES 
-    let rows = table.getRowCount();  // Count the rows in the CSV  
-    let x = 70;                      // Default x position
-    let y = 50;                      // Default y position 
+    // CREATE AND DISPLAY TIMER 
+    if (frameCount % 60 == 0) timer++; // If frames divisible by 60 1 sec has passed
+    textAlign(CENTER, CENTER);
+    text('timer: '+timer, width/2, height-50);
+    if(typeof table=="undefined" || timer>60) {
+        table = loadTable(url, 'csv', 'header');
+        timer=0;
+    }          
 
     // LOOP THROUGH CSV & DISPLAY QUAKES 
+    let rows = table.getRowCount();      // Count the rows in the CSV  
+    let x = 70;                          // Default x position
+    let y = 50;                          // Default y position 
     for (let row=0; row<rows; row++){
-
         // Access placename and magnitude variables from the CSV
         let name = table.getString(row, 'place');     // Grab placename 
         let nameArr = name.split(" of ");             // Split the placename at ' of '
         let magnitude = table.getString(row, 'mag');  // Grab the magnitute
-
         // Draw the ellipse and text label
         ellipse(x, y, magnitude*12, magnitude*12);
         text(nameArr[1], x, y+25);                    // Placename text
         text(magnitude, x, y+38);                     // Manitude text
-
         // Calculate the x,y coordinates of the next ellipse 
         x += width/3; 
         if (x > width-70){
